8885ebb8e93f5511d627fe992d68cf68e6f36ac7
Intervention7 <35593676+Intervention7@users.noreply.github.com>
2019-09-16 09:22:06 +0530

    Add files via upload

---
+++ string_op.py
@@ -0,0 +1,13 @@
+def operations():
+    s = "hello"
+
+    print("**".join(s))
+    print(s.upper())
+    print(s.lower())
+    print(s.replace('hello', 'hi'))
+    print(s.isupper())
+    print(s.islower())
+    print(s.capitalize())
+    print(s.split(' '))
+
+operations()
